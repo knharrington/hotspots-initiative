22e7466a802f99467385b2b2b9fd8299ba0c384e
Katie Harrington <knharrington@mote.org>
2024-09-13 16:36:13 -0400

    made cleaner and more functional

--- destin-app/ui.R
+++ destin-app/ui.R
@@ -32,45 +32,70 @@
    
              tabPanel(title=div(img(src="cfa-logo.png", style = "width:50px;height:50px"), "Charter Fisherman's Association"),
                       fluidPage(
+                        #shinythemes::themeSelector(),
                         sidebarLayout(
                           sidebarPanel(helpText("Use the following selections to update the data displayed on the map."),
-                                                "Total Observations:",
+                                                strong("Total Observations"),
                                                 verbatimTextOutput("text_obs", placeholder=FALSE),
                                                 p(" "),
                                                 #selectInput("region", "Region", choices = c("Destin", "Galveston"), selected="Destin"),
-                                                sliderTextInput("days", "Time Range: Days since Reporting", choices=seq(from=max(noaa_vl_des$Days_Report), to=min(noaa_vl_des$Days_Report), by=-1), selected=c(2,1), grid=TRUE),
-                                                sliderInput("effort", "Vessels Reporting", max=max(noaa_vl_des$Effort_ID), min=min(noaa_vl_des$Effort_ID), value=c(max(noaa_vl_des$Effort_ID)), step=5, round=TRUE),
+                                                sliderTextInput("days", "Days since Reporting", choices=seq(from=14, to=1, by=-1), selected=c(2,1), grid=TRUE),
+                                                #sliderInput("effort", "Vessels Reporting", max=max(noaa_vl_des$Effort_ID), min=min(noaa_vl_des$Effort_ID), value=c(max(noaa_vl_des$Effort_ID)), step=5, round=TRUE),
                                                 #checkboxGroupInput("seasons", "Season", choices = c("Spring", "Summer", "Fall", "Winter"), selected = c("Spring", "Summer", "Fall", "Winter")),
                                                 #radioButtons("show_maps", "Display Data", c("Catch Events", "Catch Per Unit Effort*"), selected = "Catch Events"),
                                                 #checkboxGroupInput("rasters", "Environmental**", choices = c("Spring", "Summer", "Fall", "Winter"), selected = NULL),
                                                 #radioButtons("grid.scale", "Catch per Unit Effort Grid Scale", choices = c("10' square" = "grid.sq10", "5' square" = "grid.sq5", "2.5' square" = "grid.sq2.5")),
-                                                actionButton("update", "Update Map")
+                                                radioGroupButtons("switch_current", "Display Current Intensity", choices = c("Yes", "No"), selected="No"),
+                                                fluidRow(
+                                                  column(width=6, radioGroupButtons("radio_depred", "Display Depredation", choices=c("Total", "Sharks", "Dolphins"), selected="Total")),
+                                                  column(width=6, radioGroupButtons("radio_layer", "Layer Style", choices=c("Intensity (grid)", "Density (heat)"), selected="Intensity (grid)"))
+                                                ),
+                                                actionButton("update", "Update Map", icon=icon("refresh"), class="btn btn-primary")
                                     ), #sidebarPanel
                                     mainPanel(
                                        tabsetPanel(
-                                         tabPanel("Map",
+                                         tabPanel("Map", icon = icon("map"),
                                            withLoader(leafletOutput("examplemap", height = "85vh"), type="html", loader="loader4")
                                            ),
-                                         tabPanel("Record New Observation",
+                                         tabPanel("Record New Observation", icon=icon("pen-to-square"),
                                            wellPanel(
                                              helpText("Please enter your observations using the following inputs."),
                                              fluidRow(
-                                               column(width=4, selectInput("select_current", label = "Current Intensity", choices = c("None", "Moderate", "High"), selected = "None")),
-                                               column(width=4, selectInput("select_depred", label = "Depredation Intensity", choices = c("None", "Moderate", "High"), selected = "None")),
-                                               column(width=4, selectInput("select_species", label = "Species Encountered", choices = c("None","Shark", "Dolphin"), selected = "None"))
+                                               column(width=4, pickerInput("select_current", label = "Current Intensity", choices = c("None", "Moderate", "High"), selected = "None",
+                                                                           choicesOpt = list(
+                                                                             content = sprintf("<span class='label label-%s'>%s</span>",
+                                                                                               c("success", "warning", "danger"), 
+                                                                                               c("None", "Moderate", "High"))
+                                                                           ))),
+                                               column(width=4, pickerInput("select_depred", label = "Depredation Intensity", choices = c("None", "Moderate", "High"), selected = "None",
+                                                                           choicesOpt = list(
+                                                                             content = sprintf("<span class='label label-%s'>%s</span>",
+                                                                                               c("success", "warning", "danger"), 
+                                                                                               c("None", "Moderate", "High"))
+                                                                           ))),
+                                               column(width=4, pickerInput("select_species", label = "Species Encountered", choices = c("None","Shark", "Dolphin"), selected = "None",
+                                                                           choicesOpt = list(
+                                                                             content = sprintf("<span class='label label-%s'>%s</span>",
+                                                                                               c("success", "primary", "info"), 
+                                                                                               c("None", "Shark", "Dolphin"))
+                                                                           )))
                                              ),
                                              fluidRow(
                                                column(width=4, radioButtons("check_loc", label = "Use Current Location", c("Yes", "No"), selected = "Yes")),
-                                               column(width=4, textInput("text_lat", label = "Latitude")),
-                                               column(width=4, textInput("text_long", label = "Longitude"))
+                                               column(width=4, 
+                                                      conditionalPanel(condition="input.check_loc == 'No'",
+                                                                       textInput("text_long", label = "Longitude"))),
+                                               column(width=4,
+                                                      conditionalPanel(condition="input.check_loc == 'No'",
+                                                      textInput("text_lat", label = "Latitude")))
                                              ), #fluidRow
                                              fluidRow(
                                                column(width=8, textInput("text_notes", label="Notes"))
                                              ),
-                                             actionButton("submit", "Submit Data")
+                                             actionButton("submit", "Submit Data", class="btn btn-info")
                                            ), #wellPanel
                                            
-                                           tableOutput("sheet_data")
+                                           DTOutput("sheet_data")
                                            
                                            ) #tabPanel 2
                                                                      #textOutput("radio.txt"),
