f2e5ead27aa9da270fdb02865d96f178215a7f7c
Katie Harrington <knharrington@mote.org>
2024-11-06 09:51:21 -0500

    add comments

--- destin-app/preprocess.R
+++ destin-app/preprocess.R
@@ -37,7 +37,7 @@
   library(digest)
 }
 
-
+# set global options for handling Google token
 options(
   gargle_oauth_email=TRUE,
   gargle_oauth_cache = "/.secrets",
@@ -54,10 +54,10 @@ sheet_id <- drive_get("cfa-test-sheet")$id
 #Data.In=read.csv("data/DestinData.csv")
 #setwd("./Destin_App2")
 #Data.In <- fread("destin-app/data/NOAA-Observer-data.csv")
+
+# Read NOAA Observer data and format columns
 Data.In <- fread("destin-app/data/NOAA-Observer-data.csv")
-
 {noaa_data=Data.In
-
 noaa_data <- noaa_data %>% dplyr::filter(COMMON_NAME != "" | SET_TIME_START != "") #| VESSEL_ID != "")
 noaa_data$SET_TIME_START_dt <- parse_date_time(noaa_data$SET_TIME_START, "d-b-y I.M.S.p")
 noaa_data$SET_TIME_END_dt <- parse_date_time(noaa_data$SET_TIME_END, "d-b-y I.M.S.p")
@@ -87,12 +87,13 @@ random_mask <- function(column) {
   return(value_map[column])
 }
 
-# data masking
+# Mask columns with sensitive information
 {noaa_data$VESSEL_ID <- random_mask(noaa_data$VESSEL_ID)
 noaa_data$TRIPNUMBER <- random_mask(noaa_data$TRIPNUMBER)
 noaa_data$UNIQUE_RETRIEVAL <- paste(noaa_data$VESSEL_ID, noaa_data$TRIPNUMBER, noaa_data$SETNUMBER, sep="_")
 }
 
+# Filter NOAA data for vertical line gear near Destin
 noaa_vl_des <- noaa_data %>%
   filter(TRIP_GEAR == 'VL' & COMMON_NAME != "NOCATCH" & LON_BEGIN_SET > -89 & LAT_BEGIN_SET > 29 & !is.na(SET_TIME_START)) %>%
   mutate(Effort_ID = dense_rank(TRIPNUMBER)) %>%
@@ -100,56 +101,37 @@ noaa_vl_des <- noaa_data %>%
   mutate(Days_Report = dense_rank(as.Date(SET_DATE, format="%m/%d/%Y"))) %>%
   ungroup()
 
-# noaa_vl_gal <- noaa_data %>%
-#   filter(TRIP_GEAR == 'VL' & COMMON_NAME != "NOCATCH" & LON_BEGIN_SET < -96.5 & LAT_BEGIN_SET > 27 & !is.na(SET_TIME_START)) %>%
-#   mutate(Effort_ID = dense_rank(TRIPNUMBER)) %>%
-#   group_by(TRIPNUMBER) %>%
-#   mutate(Days_Report = dense_rank(as.Date(SET_DATE, format="%m/%d/%Y"))) %>%
-#   ungroup()
-
-#My.metric = "Number_damaged"
-#My.met.label = "Damaged Fish Caught"
-#My.env = "Wind"  #Wind Current
-
-#options("rgdal_show_exportToProj4_warnings"="none")
 
 # #read using the sf package
 #grid.sq1 <- st_read(dsn="destin-app/shapefiles", layer = "DESTIN_GRID_NOAA_1MI")
-
 #grid.sq1 <- st_read(dsn="shapefiles", layer = "DESTIN_GRID_NOAA_1MI")
 
-grid.sq1 <- st_read(dsn="destin-app/shapefiles", layer = "destin_1mi")
-
-#grid.sq10 <- st_read(dsn = "shapefiles", layer = "GOM_GRID_10MIN_fullgulf")
-#grid.sq5 <- st_read(dsn = "shapefiles", layer = "GOM_GRID_5MIN_new")
-# grid.sq2.5 <- st_read(dsn = "shapefiles", layer = "GOM_GRID_2_5MIN")
-# #grid.hex5 <- st_read(dsn = paste0(wd,"/shapefiles"), layer = "GOM_HEX_5MIN")
-# #grid.hex2.5 <- st_read(dsn = paste0(wd,"/shapefiles"), layer = "GOM_HEX_2_5MIN")
-# 
-# # Check CRS (Coordinate Reference System)
-#st_crs(grid.sq10)
-#st_crs(grid.sq5)
+# Load shapefile for 1mi by 1mi grid
+gridshp <- st_read(dsn="destin-app/shapefiles", layer = "destin_1mi")
 
 #Need to simplify to the same columns
 #grid.sq1=grid.sq1[1] 
 #names(grid.sq1)[names(grid.sq1) == "Id"] <- "GRID_ID"
-gridshp=grid.sq1
+#gridshp=grid.sq1
 
+# Determine bounding box for shapefile limits
 bbox <- st_bbox(gridshp)
 
+# Create icon for boat and make static legend
 boat_icon <- makeIcon(iconUrl = "www/boat2.svg",
                       iconWidth=35, iconHeight=30, 
                       iconAnchorX=15, iconAnchorY=15)
 html_legend <- "<img src='boat2.svg' style='width:35px;height:30px;'> Current Location<br/>"
 
+# Read historic current speed data
 current_speed <- fread("destin-app/data/Oceanic_current_speed.csv") %>%
   dplyr::select(Longitude, Latitude, Oceanic_current_speed_October) %>%
   rename("Speed_Oct" = "Oceanic_current_speed_October")
 
-# Convert the data frame to a spatial object
+# Convert the current speed data frame to a spatial object
 current_speed_sf <- st_as_sf(current_speed, coords = c("Longitude", "Latitude"), crs = st_crs(gridshp))
 
-# Perform the spatial join
+# Perform the spatial join for current speed data and grid
 grid_join_c <- setDT(st_join(current_speed_sf, gridshp, join = st_intersects))
 
 filtered_data_c <- grid_join_c %>%
@@ -163,12 +145,14 @@ filtered_data_c <- grid_join_c %>%
     speed_mean < 0.3 ~ 1
   ))
 
+# Determine grid values for current speed data
 gridvalues_c <- st_as_sf(merge(x = gridshp, y = filtered_data_c, by = "GRID_ID", all.x = FALSE))
 #summary(current_speed)
 
 # pal = colorNumeric("Spectral", gridvalues_c$class_bin)
 # leaflet() %>% addTiles() %>% addPolygons(data=gridvalues_c, fillColor = ~pal(class_bin), color=~pal(class_bin))
 
+# create a fixed userbase and hash the passwords
 user_base <- tibble::tibble(
   user = c("cfemm-admin", "test-user"),
   password = purrr::map_chr(c("temp", "hotspots"), sodium::password_store),
@@ -176,10 +160,9 @@ user_base <- tibble::tibble(
   name = c("Admin Account", "Test User")
 )
 
-#save(gridshp, sheet_id, noaa_data, noaa_vl_des, file = "destin-app/data/preprocess.RData")
+# Save varibales for use in the server and global files
 save(gridshp, sheet_id, noaa_data, noaa_vl_des, boat_icon, html_legend, user_base, file = "destin-app/data/preprocess.RData")
 
-#saveRDS(user_base, "./destin-app/data/user_base.rds")
 ################################################################################
 ### Non-reactive world
 
