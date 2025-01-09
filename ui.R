3e43d56125b9a1a76c1806a7943aa1114e3939d5
Katie Harrington <knharrington@mote.org>
2025-01-09 12:18:31 -0500

    remove old comments and code

--- ui.R
+++ ui.R
@@ -1,12 +1,7 @@
 ################################################################################
-# This script builds the user interface for the destin app
+# This script builds the user interface for the Destin CFA app
 ################################################################################
 
-# mytheme <- create_theme(
-#   adminlte_color(
-#     aqua = "navy"
-#   )
-# )
 
 dashboardPage(skin="black",
               
@@ -80,10 +75,7 @@ dashboardPage(skin="black",
         fluidRow(
           box(
             width=12, title = "Map", status="primary", solidHeader=TRUE, collapsible=FALSE, #height=800,
-            withLoader(leafletOutput("examplemap", height = 740), type="html", loader="loader4")#,
-            # DTOutput("trouble_table"),
-            # DTOutput("trouble_table2"),
-            # DTOutput("trouble_table3")
+            withLoader(leafletOutput("examplemap", height = 740), type="html", loader="loader4")
           ) # box -map
         ) # fluid row
       ), #tab item
@@ -128,9 +120,6 @@ dashboardPage(skin="black",
           actionButton("submit", "Submit Data", class="btn btn-primary", style = "color: white;")
         )#, # box
         ) # fluidrow
-        # box(
-        #   width=4, status="info",
-        # )
       ), #tab item
       tabItem(tabName="usertab",
         fluidRow(
@@ -151,76 +140,3 @@ dashboardPage(skin="black",
   ) # dashboard body
 ) #dashboard page  
 
-################ OLD UI ##################
-#                 tabPanel(
-#                       fluidPage(
-#                         #shinythemes::themeSelector(),
-#                         sidebarLayout(
-#                           sidebarPanel(helpText("Use the following selections to update the data displayed on the map."),
-#                                                 strong("Total Observations"),
-#                                                 verbatimTextOutput("text_obs", placeholder=FALSE),
-#                                                 p(" "),
-#                                                 sliderTextInput("days", "Days since Reporting", choices=seq(from=1, to=14, by=1), selected=c(3), grid=TRUE),
-#                                                 radioGroupButtons("radio_current", "Display Current Intensity", choices = c("Yes", "No"), selected="No"),
-#                                                 fluidRow(
-#                                                   column(width=6, radioGroupButtons("radio_depred", "Display Depredation", choices=c("Total", "Sharks", "Dolphins"), selected="Total")),
-#                                                   column(width=6, radioGroupButtons("radio_layer", "Layer Style", choices=c("Intensity (grid)", "Density (heat)"), selected="Intensity (grid)"))
-#                                                 ),
-#                                                 actionButton("update", "Update Map", icon=icon("refresh"), class="btn btn-primary")
-#                                     ), #sidebarPanel
-#                                     mainPanel(
-#                                        tabsetPanel(
-#                                          tabPanel("Map", icon = icon("map"),
-#                                            withLoader(leafletOutput("examplemap", height = "85vh"), type="html", loader="loader4")
-#                                            ),
-#                                          tabPanel("Record New Observation", icon=icon("pen-to-square"),
-#                                            wellPanel(
-#                                              helpText("Please enter your observations using the following inputs."),
-#                                              fluidRow(
-#                                                column(width=4, pickerInput("select_current", label = "Current Intensity", choices = c("None", "Moderate", "High"), selected = "None",
-#                                                                            choicesOpt = list(
-#                                                                              content = sprintf("<span class='label label-%s'>%s</span>",
-#                                                                                                c("success", "warning", "danger"), 
-#                                                                                                c("None", "Moderate", "High"))
-#                                                                            ))),
-#                                                column(width=4, pickerInput("select_depred", label = "Depredation Intensity", choices = c("None", "Moderate", "High"), selected = "None",
-#                                                                            choicesOpt = list(
-#                                                                              content = sprintf("<span class='label label-%s'>%s</span>",
-#                                                                                                c("success", "warning", "danger"), 
-#                                                                                                c("None", "Moderate", "High"))
-#                                                                            ))),
-#                                                column(width=4, pickerInput("select_species", label = "Species Encountered", choices = c("None","Shark", "Dolphin"), selected = "None",
-#                                                                            choicesOpt = list(
-#                                                                              content = sprintf("<span class='label label-%s'>%s</span>",
-#                                                                                                c("success", "primary", "info"), 
-#                                                                                                c("None", "Shark", "Dolphin"))
-#                                                                            )))
-#                                              ),
-#                                              fluidRow(
-#                                                column(width=4, radioButtons("check_loc", label = "Use Current Location", c("Yes", "No"), selected = "Yes")),
-#                                                column(width=4, 
-#                                                       conditionalPanel(condition="input.check_loc == 'No'",
-#                                                                        textInput("text_long", label = "Longitude"))),
-#                                                column(width=4,
-#                                                       conditionalPanel(condition="input.check_loc == 'No'",
-#                                                       textInput("text_lat", label = "Latitude")))
-#                                              ), #fluidRow
-#                                              fluidRow(
-#                                                column(width=8, textInput("text_notes", label="Notes"))
-#                                              ),
-#                                              actionButton("submit", "Submit Data", class="btn btn-info")
-#                                            )#, #wellPanel
-#                                            
-#                                            #DTOutput("sheet_data")
-#                                            
-#                                            ), #tabPanel 2
-#                                          tabPanel("User Data", icon=icon("user"),
-#                                                   
-#                                             DTOutput("user_data") 
-#                                          ) #tabPanel 3
-#                                         ) #tabsetPanel
-#                                      ) #mainPanel
-#                                    ) #sidebarLayout
-#                         ) # fluidPage
-#                       
-#                     )#, #tabPanel
